9bc140f72152de1368fee8c8f6742c9d3b1341e5
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-03-17 16:28:28 -0700

    commit tree fed8d67f1c334e6ff4f9fc0259d0e40ad80713b9

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,18 @@
-// your Bomb code here!
+import React, { Component } from "react";
+export default class Bomb extends Component {
+  state = {
+    secondsLeft: this.props.initialCount,
+  };
+
+  renderSeconds = () => {
+    if (this.state.secondsLeft === 0) {
+      return "Boom!";
+    } else {
+      return `${this.state.secondsLeft} seconds left before I go boom!`;
+    }
+  };
+
+  render() {
+    return <div>{this.renderSeconds()}</div>;
+  }
+} 
